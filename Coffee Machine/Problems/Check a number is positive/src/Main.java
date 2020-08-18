246f2950823018283e5ff4cdc5a428d64042901b
P-Brzezinski <pawelbrzezinski1987@gmail.com>
2020-08-18 21:42:22 +0200

    Coffe machine project finished

--- Coffee Machine/Problems/Check a number is positive/src/Main.java
+++ Coffee Machine/Problems/Check a number is positive/src/Main.java
@@ -1,9 +1,5 @@
 class Main {
     public static void main(String[] args) {
         // put your code here
-
-        Scanner scanner = new Scanner(System.in);
-
-        int number = scanner.
     }
 }
--- Coffee Machine/Problems/Right Rotation/src/Main.java
+++ Coffee Machine/Problems/Right Rotation/src/Main.java
@@ -1,28 +1,5 @@
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         // put your code here
-
-        Scanner scanner = new Scanner(System.in);
-
-        int[] tab = new int[5];
-
-        for (int i = 0; i < tab.length; i++) {
-            tab[i] = scanner.nextInt();
-        }
-
-        int rotate = scanner.nextInt();
-        int i = 0;
-
-        tab[i + 1] = tab[i];
-        tab[i + 2] = tab[i + 1];
-        tab[i + 3] = tab[i + 2];
-        tab[i + 4] = tab[i + 3];
-        tab[i] = tab[i + 4];
-
-        for (int j : tab) {
-            System.out.print(j + " ");
-        }
     }
 }
